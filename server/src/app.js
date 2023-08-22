ebb26e2f3511ee494f65c95ff0f0c0fb5d44bf18
Manthanar30 <maungthong6041@gmail.com>
2023-08-23 02:14:08 +0700

    nnn

--- server/src/app.js
+++ server/src/app.js
@@ -1,55 +1,23 @@
 let express = require('express')
 let bodyParser = require('body-parser')
-const {sequelize} = require('./models')
-
-const config = require('./config/config')
 
 const app = express()
- 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
 
-require('./routes')(app)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+require('./route')(app)
 
 app.get('/status', function (req,res){
-    res.send('Hello nodejs server belong to manthana')
+    res.send('Hello nodejs server belong to Songwut')
 })
 
-app.get('/hello/:name', function (req,res) {
+app.get('/hello/:name', function(req,res){
     console.log('hello - ' + req.params.name)
-    res.send('sey hello with ' + req.params.name)
-   })
-
-   app.get('/user/:userId', function (req, res) {
-    res.send('ดูข้อมูลผู้ใช้งาน'+ req.params.userId)
-   })
-
-   app.get('/user/:users', function (req, res) {
-    res.send('ดูข้อมูลผู้ใช้งาน')
-   })
-
-   app.post('/user/', function (req, res) {
-    res.send('สร้างผู้ใช้งาน' + JSON.stringify(req.body))
-   })
-
-   app.put('/user/:userId', function (req, res) {
-    res.send('แก้ไขข้อมูลผู้ใช้' + req.params.userId + ' : ' +
-   JSON.stringify(req.body.name))
-   })
-
-   app.delete('/user/:userId', function (req, res) {
-    res.send('ลบผุ้ใช้งาน' + req.params.userId + ' : ' + 
-    JSON.stringify(req.body.name))
+    res.send('say hello with ' + req.params.name)
 })
 
-let port = process.env.PORT || config.port
-
-sequelize.sync({force: false}).then(() => {
-    app.listen(port, function () {
-    console.log('Server running on ' + port)
-    })
-})
-
+let port = 8081
 app.listen(port, function(){
-    console.log('server running on ' + port)
-})
+    console.log('server runing on ' + port)
+})  
